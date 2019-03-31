2508e9cd22e99ad3d8e31b77604913d1f8d29714
Nguyễn Văn Minh <48230406+MinhNguyenK56cnnt@users.noreply.github.com>
2019-03-31 12:25:01 +0700

    project mô hình mvvm pm quản lý kho
    project mô hình mvvm pm quản lý kho

---
+++ 31_3_2019_tạo project chuẩn mvvm/PMquanlykho/PMquanlykho/ViewModel/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using PMquanlykho.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace PMquanlykho.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+            MessageBox.Show("Đã vào trong MainViewModel -> DataContext của mainwindow.xaml");
+        }
+    }
+}
