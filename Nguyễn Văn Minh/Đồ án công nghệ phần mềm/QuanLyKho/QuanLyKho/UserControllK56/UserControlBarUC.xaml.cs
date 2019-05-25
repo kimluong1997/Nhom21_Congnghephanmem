d4c55a04dbe1f5e531b64610ca44a3ae9be37750
Nguyễn Văn Minh <48230406+MinhNguyenK56cnnt@users.noreply.github.com>
2019-05-25 08:09:32 +0700

    Thiết kế xong giao diện và controlbar
    Thiết kế xong giao diện và controlbar

---
+++ Nguyễn Văn Minh/Đồ án công nghệ phần mềm/QuanLyKho/QuanLyKho/UserControllK56/UserControlBarUC.xaml.cs
@@ -0,0 +1,31 @@
+﻿using QuanLyKho.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuanLyKho.UserControllK56
+{
+    /// <summary>
+    /// Interaction logic for UserControlBarUC.xaml
+    /// </summary>
+    public partial class UserControlBarUC : UserControl
+    {
+        public ControlBarViewModel viewmodel { get; set; }       
+        public UserControlBarUC()
+        {
+            InitializeComponent();
+            this.DataContext = viewmodel = new ControlBarViewModel();
+        }
+    }
+}
--- Nguyễn Văn Minh/Đồ án công nghệ phần mềm/QuanLyKho/QuanLyKho/ViewModel/MainViewModel.cs
+++ Nguyễn Văn Minh/Đồ án công nghệ phần mềm/QuanLyKho/QuanLyKho/ViewModel/MainViewModel.cs
@@ -4,15 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace QuanLyKho.ViewModel
 {
     public class MainViewModel : BaseViewModel
     {
+        public bool IsLoaded = false;
+        public ICommand LoadedWindowCommand { get; set; }
         // mọi thứ xử lý sẽ nằm trong này
         public MainViewModel()
         {
-            MessageBox.Show("Đã vào trong MainViewModel -> DataContext của mainwindow.xaml");
+            if(!IsLoaded)
+            {
+                LoadedWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+                {
+                    IsLoaded = true;
+                    LoginWindow login = new LoginWindow();
+                    login.ShowDialog();
+                });
+            }
+           
         }
     }
 }
