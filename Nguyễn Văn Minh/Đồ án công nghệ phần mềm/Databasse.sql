098d95eaf553e4a616329da09f4ebb6f1d927535
Nguyễn Văn Minh <48230406+MinhNguyenK56cnnt@users.noreply.github.com>
2019-05-26 08:19:38 +0700

    database_hoanf chinhr
    database hoàn chỉnh của dự án

--- Nguyễn Văn Minh/Đồ án công nghệ phần mềm/Databasse.sql
+++ Nguyễn Văn Minh/Đồ án công nghệ phần mềm/Databasse.sql
@@ -32,9 +32,13 @@ ContactDate DateTime
 create table UserRoles
 (
 Id int identity(1,1) primary key,
-DisplayName nvarchar(max),
-
+DisplayName nvarchar(max)
 )
+insert into UserRoles(DisplayName) values(N'Admin')
+go
+insert into UserRoles(DisplayName) values(N'Nhan vien')
+go
+
 create table Users
 (
 Id int identity(1,1) primary key,
@@ -66,11 +70,11 @@ create table InputInfos
 (
 Id nvarchar(128) primary key,
 IdObject nvarchar(128) not null,
-IdInput nvarchar(28) not null,
+IdInput nvarchar(128) not null,
 Counts int,
 InputPrice float default 0,
-OutputPrice float default 0
-
+OutputPrice float default 0,
+Status nvarchar(max),
 foreign key(IdObject) references Objects(Id),
 foreign key(IdInput) references Inputs(Id)
 )
@@ -85,12 +89,12 @@ create table OutputInfos
 (
 Id nvarchar(128) primary key,
 IdObject nvarchar(128) not null,
-IdOutputInfo nvarchar(28) not null,
+IdInputInfo nvarchar(128) not null,
 IdCustomer int not null,
 Counts int,
-InputPrice float default 0,
-OutputPrice float default 0
-
+Status nvarchar(max),
 foreign key(IdObject) references Objects(Id),
-foreign key(IdInput) references Inputs(Id)
+foreign key(IdInputInfo) references InputInfos(Id),
+foreign key(IdCustomer) references Customers(Id)
 )
+
